d68a6701ddc5872895425455500cba8f16bf635d
Lidiane Trindade <47565259+lstrindade@users.noreply.github.com>
2022-11-16 15:37:17 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+
+let engine;
+let world;
+
+var ground;
+var boxA, boxB;
+var mouse, mouseConstraint;
+
+
+function setup() 
+{
+  
+  engine = Engine.create();
+  world = engine.world;
+
+   
+  
+
+} 
+
+
